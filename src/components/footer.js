ee72f2500c7ca828656b7f55426b15a007063886
Drake Hobson <drakehobson2013@gmail.com>
2021-03-24 14:44:48 -0400

    commit pre pull

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,15 +1,12 @@
 import React from 'react'
-import Github from './Images/GitHub_logo.png';
+import Linktree from './Images/Linktree.png';
 
 
 const footer = () => {
     return (
         
 <footer class="text-center fixed-bottom">
-<a className="gitLink" href="https://github.com/drakehobson2013"><img className="gitImg" src={Github} alt=""/></a>
-<a className="gitLink" href="https://github.com/drakehobson2013"><img className="gitImg" src={Github} alt=""/></a>
-<a className="gitLink" href="https://github.com/drakehobson2013"><img className="gitImg" src={Github} alt=""/></a>
-<a className="gitLink" href="https://github.com/drakehobson2013"><img className="gitImg" src={Github} alt=""/></a>
+<a className="gitLink" href="https://github.com/drakehobson2013"><img className="gitImg" src={Linktree} alt=""/></a>
   <p className="w3-medium text-white">Follow us on GitHub | ThisIsCincy©2021</p>
 </footer>
             
